29b78fc677bbace71fbad5988726c9b3a944876f
rgutmed873-droid <rgutmed873@iesgrancapitan.org>
2026-02-17 20:04:18 +0100

    Clases test y bustest terminada

--- src/main/java/DAO/BusDAO.java
+++ src/main/java/DAO/BusDAO.java
@@ -5,7 +5,9 @@ import modelos.Bus;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BusDAO {
 
@@ -32,5 +34,62 @@ public class BusDAO {
            e.printStackTrace();
            return false;
        }
+    }
+    public static Bus findBus(Connection con, String registro) {
+
+        // Sentencia SQL parametrizada para buscar un bus por su registro
+        String sql = "SELECT * FROM Bus WHERE registro = ?;";
+
+
+        // Prepara la sentencia SQL
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, registro);
+
+
+            // Ejecuta la consulta y obtiene el resultado
+            try (ResultSet rs = ps.executeQuery()) {
+
+
+                // Si se encuentra un registro, crea y devuelve el objeto Bus
+                if (rs.next()) {
+                    String registroBus = rs.getString("registro");
+                    String tipo = rs.getString("tipo");
+                    String licencia = rs.getString("licencia");
+                    return new Bus(registroBus, tipo, licencia);
+                }
+            }
+        } catch (SQLException e) {
+            // Imprime información de error si ocurre una excepción SQL
+            e.printStackTrace();
+        }
+        // Devuelve null si no se encontró el bus o hubo algún error
+        return null;
+    }
+
+    public static ArrayList<Bus> consultBus(Connection con) throws SQLException {
+
+        String sqlConsultaBus = "SELECT  * FROM Bus";
+        ArrayList<Bus> buses = new ArrayList<>();
+        try (PreparedStatement ps = con.prepareStatement(sqlConsultaBus)){
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+
+                Bus busConsult = new Bus();
+
+                busConsult.setRegistro(rs.getString("registro"));
+                busConsult.setTipo(rs.getString("tipo"));
+                busConsult.setLicencia(rs.getString("licencia"));
+
+                buses.add(busConsult);
+            }
+            return buses;
+
+        } catch (SQLException e) {
+            System.out.println("Error en la consulta de BBDD");
+            throw new RuntimeException(e);
+        }
+
     }
 }
---
+++ src/test/java/BusDAOTest.java
@@ -0,0 +1,132 @@
+import DAO.BusDAO;
+import modelos.Bus;
+import org.junit.jupiter.api.*;
+
+import java.sql.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BusDAOTest {
+    private static Connection con;
+
+    /**
+     * Test para realizar la conexión con la base de datos
+     * @throws SQLException
+     */
+    @BeforeAll
+    static void conectar() throws SQLException {
+        con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/AUCORSA",
+                "root",
+                ""
+        );
+    }
+
+    /**
+     * Metodo para cerrar la base datos
+     * @throws SQLException
+     */
+    @AfterAll
+    static void cerrar() throws SQLException {
+        if (con != null) con.close();
+    }
+
+    /**
+     * Metodo para borras las tablas
+     * @throws SQLException
+     */
+    @BeforeEach
+    void limpiarTabla() throws SQLException {
+        Statement st = con.createStatement();
+        st.executeUpdate("DELETE FROM Routes");
+        st.executeUpdate("DELETE FROM Bus");
+    }
+
+    /**
+     * Metodo test para insertar un bus en la tabla de bus
+     * @throws SQLException
+     */
+    @Test
+    @Order(1)
+    @DisplayName("Insertar Bus en la BD")
+    void testInsertarBus() throws SQLException {
+
+        //CAMBIAR LOS DATOS
+        PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO Bus VALUES (?, ?, ?)"
+        );
+        ps.setString(1, "B111");
+        ps.setString(2, "Urbano");
+        ps.setString(3, "LIC111");
+
+
+        int filas = ps.executeUpdate();
+
+
+        assertEquals(1, filas);
+    }
+
+    /**
+     * Test para buscar un bus existente
+     * @throws SQLException
+     */
+    @Test
+    @Order(2)
+    @DisplayName("Buscar bus existente")
+    void testBuscarBusExistente() throws SQLException {
+
+
+        con.createStatement().executeUpdate(
+                "INSERT INTO Bus VALUES ('B222','Escolar','LIC222')"
+        );
+
+
+        Bus bus = BusDAO.findBus(con,"B333");
+
+
+        assertNotNull(bus);
+        assertEquals("B222", bus.getRegistro());
+        assertEquals("Escolar", bus.getTipo());
+        assertEquals("LIC222", bus.getLicencia());
+    }
+
+    /**
+     * Test para buscar un bus inexistente
+     */
+    @Test
+    @Order(3)
+    @DisplayName("Buscar bus inexistente devuelve null")
+    void testBuscarBusInexistente() {
+
+
+        Bus bus = BusDAO.findBus(con, "NOEXISTE");
+
+
+        assertNull(bus);
+    }
+
+    /**
+     * Test para mostrar todos los buses
+     * @throws SQLException
+     */
+    @Test
+    @Order(4)
+    @DisplayName("Mostrar todos los buses")
+    void testMostrarTodosLosBuses() throws SQLException {
+
+
+        con.createStatement().executeUpdate(
+                "INSERT INTO Bus VALUES ('B333','Turismo','LIC333')");
+        con.createStatement().executeUpdate(
+                "INSERT INTO Bus VALUES ('B444','Urbano','LIC444')");
+
+
+        List<Bus> lista = BusDAO.consultBus(con);
+
+
+        assertNotNull(lista);
+        assertEquals(2, lista.size());
+    }
+
+}
